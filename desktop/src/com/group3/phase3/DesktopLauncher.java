2d9e6748cf6660fa9653ac2045b3ea615e6cd060
João Victor <j.coutinhoviragine@student.maastrichtuniversity.nl>
2023-06-12 18:06:10 +0200

    Creating LibGDX Files

---
+++ desktop/src/com/group3/phase3/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.group3.phase3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.group3.phase3.RunSimulation;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Landing Simulation");
+		new Lwjgl3Application(new RunSimulation(), config);
+	}
+}
